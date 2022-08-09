40b7b03f73c7c6278439a96cd5d2a7b1834db67a
= <=>
2022-08-09 22:11:36 +0100

    made changessss

--- scripts/cribsController.js
+++ scripts/cribsController.js
@@ -1,11 +1,48 @@
 myApp.controller('cribsController', function($scope, cribsFactory){
     // $scope.cribs = cribsFactory.getCribs()
-    $scope.cribs;
+    $scope.model = {};
+    $scope.model.cribs = [];
+    $scope.model.filtered_cribs = false;
+    $scope.priceInfo = {
+        min: '0',
+        max: '1000000'
+
+    };
+
+    $scope.model.newListing = {};
+    
+
+    $scope.model.addCrib = function(){
+        $scope.model.newListing.image = 'default-crib.jpg';
+        $scope.model.cribs.push($scope.model.newListing);
+        $scope.model.newListing = {};
+        // console.log($scope.model.cribs) 
+    }
+
 
     cribsFactory.getCribs().success(function(data){
-        $scope.cribs = data;
+        // console.log(data, "datee")
+
+        $scope.model.cribs = data
     }).error(function(error){
         console.log(error)
     })
 
-})
+
+    
+    $scope.model.filterByPrice = function(){
+        var filtered = [];
+
+        var min = $scope.priceInfo.min;
+        var max = $scope.priceInfo.max;
+
+       
+        angular.forEach($scope.model.cribs, function(listing){
+            if(listing.price >= min && listing.price <= max){
+                filtered.push(listing);
+            }
+        });
+        return filtered;
+    }
+
+});
